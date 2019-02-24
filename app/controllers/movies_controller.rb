a75d30626ab005b702f3a67633019d49b76b2300
clarrea <clarrea@uccs.edu>
2019-02-24 19:41:35 +0000

    sort by Movie Title

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,11 +7,27 @@ class MoviesController < ApplicationController
   def show
     id = params[:id] # retrieve movie ID from URI route
     @movie = Movie.find(id) # look up movie by unique ID
+    @type = 'hilite'
     # will render app/views/movies/show.<extension> by default
   end
 
   def index
-    @movies = Movie.all
+		if params.key?(:sort_by)
+			session[:sort_by] = params[:sort_by]
+		elsif session.key?(:sort_by)
+			params[:sort_by] = session[:sort_by]
+			redirect_to movies_path(params) and return
+		end
+		@hilite = sort_by = session[:sort_by]
+		@all_ratings = Movie.all_ratings
+		if params.key?(:ratings)
+			session[:ratings] = params[:ratings]
+		elsif session.key?(:ratings)
+			params[:ratings] = session[:ratings]
+			redirect_to movies_path(params) and return
+		end
+		@checked_ratings = (session[:ratings].keys if session.key?(:ratings)) || @all_ratings
+    @movies = Movie.order(sort_by).where(rating: @checked_ratings)
   end
 
   def new
